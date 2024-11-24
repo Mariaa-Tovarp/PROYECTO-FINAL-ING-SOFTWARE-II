1e74da33e7d6bcee4d2796c1e0463cf325ef3418
Mariaa-Tovarp <maria.tovar.ttc.406@unilibre.edu.co>
2024-11-24 18:00:14 -0500

    Proyecto Final

---
+++ CODIGO PROYECTO/src/main/java/com/mycompany/prototipos/VacasI.java
@@ -0,0 +1,51 @@
+
+package com.mycompany.prototipos;
+
+
+public class VacasI {
+    
+    String nombre,raza,edad,fechanac,periapa,lactan,peso;
+
+    public VacasI(String nombre, String raza, String edad, String fechanac, String periapa, String lactan, String peso) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+        this.fechanac = fechanac;
+        this.periapa = periapa;
+        this.lactan = lactan;
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public String getFechanac() {
+        return fechanac;
+    }
+
+    public String getPeriapa() {
+        return periapa;
+    }
+
+    public String getLactan() {
+        return lactan;
+    }
+
+    public String getPeso() {
+        return peso;
+    }
+
+
+    
+    
+    
+}
